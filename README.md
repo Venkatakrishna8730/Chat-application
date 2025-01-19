45bd3b1fbf489e287c1fe31bc0bbd264ef35fd31
Venkatakrishna <myana.venkatakrishna2006@gmail.com>
2025-01-19 20:20:10 +0530

    Deployment

---
+++ README.md
@@ -0,0 +1,19 @@
+### Setup .env file
+
+```js
+PORT=...
+MONGO_URI=...
+JWT_SECRET=...
+```
+
+### Build the app
+
+```shell
+npm run build
+```
+
+### Start the app
+
+```shell
+npm start
+```
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -11,6 +12,8 @@ import cookieParser from "cookie-parser";
 
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
@@ -22,9 +25,14 @@ app.use(
 );
 const PORT = process.env.PORT || 8000;
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
