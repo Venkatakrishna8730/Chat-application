d6e81476d88e6b0878a4822745751e6af7305496
Venkatakrishna <myana.venkatakrishna2006@gmail.com>
2025-05-16 18:38:13 +0530

    Done

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,9 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // allow your frontend
-    methods: ["GET", "POST"], // allow methods
-    credentials: true, // if needed
+    origin: true,
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
